737c837706d73647919501e8a7cb85136a1c5e40
Binay Maharjan <binaya_m@jobins.jp>
2024-03-04 10:29:55 +0545

    add link to checkout

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -131,12 +131,12 @@
             </div>
 
             <div class="mt-10 flex">
-              <button
-                type="submit"
+              <RouterLink
+                to="/checkout"
                 class="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
               >
                 Add to bag
-              </button>
+              </RouterLink>
 
               <button
                 type="button"
